f5c4cc9d18c3ee19c4ecbe9d87f5bae792663916
Eoin Rodrigues <156860818+ARandomGuyTH@users.noreply.github.com>
2024-09-25 12:25:24 +0100

    Update pieces.py
    added all piece classes

--- pieces.py
+++ pieces.py
@@ -1,6 +1,20 @@
-class piece:
+class Piece:
   def __init__(self, colour):
     self.colour = colour
 
+class Pawn(Piece):
+  pass
+
+class Knight(Piece):
+  pass
+
+class Rook(Piece):
+  pass
+
+class Queen(Piece):
+  pass
+
+class King(Piece):
+  pass
     
   
