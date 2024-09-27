00c7fb75e39f50b360891e1b2b399769e55acde4
Eoin Rodrigues <156860818+ARandomGuyTH@users.noreply.github.com>
2024-09-27 10:28:09 +0100

    Update ChessRunner.py
    added comments

--- ChessRunner.py
+++ ChessRunner.py
@@ -15,6 +15,7 @@ BLACK = False
 #FEN chess notation for a starting board (means if INIT_SEQUENCE is changed the program can be used for any game state i.e. puzzles)
 #White pieces are uppercase, black pieces are lower case
 INIT_SEQUENCE = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
+#Sequence map used for creating objects using FEN
 SEQUENCE_MAP = {
   "r" : pieces.Rook,
   "n" : pieces.Knight,
@@ -30,9 +31,11 @@ for y, rank in enumerate(INIT_SEQUENCE.split("/")):
   temp_rank_list = [None for x in range(8)]
   #iterates through the individual pieces
   for x, piece in enumerate(rank):
+    #checks if a piece or value
     if piece.isnumeric():
       x += int(piece)
     else:
+      #creates and object if upper case piece is white
       temp_rank_list[x] = SEQUENCE_MAP[piece.lower()](colour = piece.isupper(), position = [x, y])
   board.append(temp_rank_list)  
 
