4b8bfa55ed426ba6ef4a55b98469610eb3e00ef1
Eoin Rodrigues <156860818+ARandomGuyTH@users.noreply.github.com>
2024-10-09 09:30:45 +0100

    Update ChessRunner.py

--- ChessRunner.py
+++ ChessRunner.py
@@ -1,6 +1,22 @@
-import pygame
-import sys
 import pieces
+
+def create_board(FEN):
+  board = []
+  #position is (x,y) or (rank, file)
+  #iterates through the ranks
+  for y, rank in enumerate(INIT_SEQUENCE.split("/")):
+    temp_rank_list = [None for x in range(8)]
+    #iterates through the individual pieces
+    for x, piece in enumerate(rank):
+      #checks if a piece or value
+      if piece.isnumeric():
+        x += int(piece)
+      else:
+        #creates and object if upper case piece is white
+        temp_rank_list[x] = SEQUENCE_MAP[piece.lower()](colour = piece.isupper(), position = [x, y])
+    board.append(temp_rank_list)   
+  return board
+
 
 #creates game UI
 pygame.init()
@@ -8,7 +24,6 @@ SCREEN_SIZE = (600, 600)
 screen = pygame.display.set_mode(SCREEN_SIZE)
 
 #creates game board
-board = []
 WHITE = True
 BLACK = False
 
@@ -25,26 +40,4 @@ SEQUENCE_MAP = {
   "p" : pieces.Pawn
 }
 
-#position is (x,y) or (rank, file)
-#iterates through the ranks
-for y, rank in enumerate(INIT_SEQUENCE.split("/")):
-  temp_rank_list = [None for x in range(8)]
-  #iterates through the individual pieces
-  for x, piece in enumerate(rank):
-    #checks if a piece or value
-    if piece.isnumeric():
-      x += int(piece)
-    else:
-      #creates and object if upper case piece is white
-      temp_rank_list[x] = SEQUENCE_MAP[piece.lower()](colour = piece.isupper(), position = [x, y])
-  board.append(temp_rank_list)  
-
-
-#game loop
-while True:
-  
-  #checks if a player has exited the game
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      sys.exit()
-    
+board = create_board(INIT_SEQUENCE)
