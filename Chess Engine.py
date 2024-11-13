e2829bf1d6e264ea52d20e26e6f7fa7a6604c474
Eoin Rodrigues <156860818+ARandomGuyTH@users.noreply.github.com>
2024-11-13 09:51:57 +0000

    added update board function

--- Chess Engine.py
+++ Chess Engine.py
@@ -42,12 +42,26 @@ class Board:
       for piece in rank:
         print(piece, end="|")
       print()
+  
+  #given a move ((x,y), (x,y)) move that piece
+  #move should be a tuple of 2 tuples. First tuple
+  def update_board(self, move):
+    """
+    given a move ((x,y), (x,y)) move that piece.
+    move should be a tuple of 2 tuples.
+    First tuple, coord of piece to move. Second, coord of square to move it to.
+    top left 0,0. bottom right 8,8.
+    """
+    self.board[move[0][0]][move[0][1]], self.board[move[1][1]][move[1][0]] = self.board[move[1][1]][move[1][0]], self.board[move[0][0]][move[0][1]]
 
-
+  
 #FEN chess notation for a starting board (means if INIT_SEQUENCE is changed the program can be used for any game state i.e. puzzles)
 #White pieces are uppercase, black pieces are lower case
 INIT_SEQUENCE = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR"
 
 board = Board(INIT_SEQUENCE)
 
+board.print_board()
+
+board.update_board(((1,1), (1,3)))
 board.print_board()
