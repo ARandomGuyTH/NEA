6c7b70700edf73d67e426c867e9b6b01037335d7
Eoin Rodrigues <156860818+ARandomGuyTH@users.noreply.github.com>
2024-11-15 19:15:24 +0000

    added pygame stuff

--- chessgame.py
+++ chessgame.py
@@ -7,7 +7,7 @@ chess_board = chessengine.Board(chessengine.default_FEN)
 
 #creates game screen
 pygame.init()
-SCREEN_SIZE = (600, 600)
+SCREEN_SIZE = WIDTH, HEIGHT = (600, 600)
 screen = pygame.display.set_mode(SCREEN_SIZE)
 screen.fill("white")
 
@@ -20,35 +20,21 @@ background_rect = background.get_rect(topleft=(0,0))
 background.fill("white")
 
 
-#image=pygame.image.load("blank.png").convert_alpha()
-#image=pygame.transform.scale(image,(165,165))
+def draw_board():
+  #creates the squares on the board
+  square_size = WIDTH / 8
+  squares = []
+  for i in range(8):
+    row = []
+    for j in range(8):
+      #creates rectangle
+      rect = pygame.Rect( j * square_size, i * square_size, square_size, square_size)
+      #draws the rectangle onto the screen
+      pygame.draw.rect(screen, "white" if (i + j) % 2 == 0 else "black", rect)
+  
+      #draws the pieces onto the board
+      
 
-class Square(pygame.sprite.Sprite):
-  def __init__(self, pos, coord):
-    super().__init__()
-    #self.image = image
-    #self.rect=self.image.get_rect(bottomright=pos)
-    #self.value=-1
-    #self.coord=coord
-    #cross=pygame.image.load("cross.png").convert_alpha()
-    #cross=pygame.transform.scale(cross,(165,165))
-    #circle=pygame.image.load("circle.png").convert_alpha()
-    #circle=pygame.transform.scale(circle,(165,165))
-    #self.img_list=[cross,circle]
-
-def update(self):
-   pass
-
-#creates squares group that will be used for mouse controls
-#by using a group I can update all squares at once
-Squares = pygame.sprite.Group()
-
-#creates squares on board
-for x in range(8):
-  for y in range(8):
-    #doesnt work rn lol
-    #Squares.add(Square((x*-170+555,y*-170+555),(x,y)))
-    pass
 
 def main():
     #game loop
@@ -57,12 +43,10 @@ def main():
         for event in pygame.event.get():
             #checks if the player has exited the game
             if event.type == pygame.QUIT:
-                pygame.exit()
                 exit()
+        
+        draw_board()
 
-        #updates all squares
-        Squares.update()
-        Squares.draw(screen)
 
         pygame.display.update()
         #DeltaTime is the time between frames, will be used for timing
--- pieces.py
+++ pieces.py
@@ -1,32 +1,39 @@
 #default class that all pieces inherit from
 class Piece:
-  def __init__(self, colour, position):
+  image : str
+  def __init__(self, colour : bool, position : list):
     self.colour = colour #piece colour
     self.position = position #keeps track of the position of the piece on the board
-  
+    self.pp = self.image[0] if self.colour else self.image[1]
+
   def __str__(self):
     #determines the colour of the piece
     if self.colour:
       colour = "white"
     else:
       colour = "black"
-      
-    return f"{colour} {self.__class__.__name__}" #returns the colour and piece as a f string
+    
+    return f"{self.pp}"
+    #return f"{colour} {self.__class__.__name__}" #returns the colour and piece as a f string
     
 class Pawn(Piece):
-  pass
+  #class variable to store the filepath of the image.
+  image = ("hi", "sus")
 
 class Knight(Piece):
-  pass
+  #class variable to store the filepath of the image.
+  image = ("hi", "sus")
 
 class Rook(Piece):
-  pass
-
+  #class variable to store the filepath of the image.
+  image = ("hi", "sus")
 class Bishop(Piece):
-  pass
-
+  #class variable to store the filepath of the image.
+  image = ("hi", "sus")
 class Queen(Piece):
-  pass
+  #class variable to store the filepath of the image.
+  image = ("hi", "sus")
 
 class King(Piece):
-  pass
+  #class variable to store the filepath of the image.
+  image = ("hi", "sus")
